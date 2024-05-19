6f39b920a5def761dc5009a4d1b3d923ea25b40f
JulianScarr <ljjackson@st.ug.edu.gh>
2024-05-19 16:11:00 +0000

    Task three accomplished

---
+++ userInfo.js
@@ -0,0 +1,21 @@
+unction createUserProfiles(names, modifiedNames) {
+    const userProfiles = [];
+    let id = 1;
+    for (let i = 0; i < names.length; i++) {
+      const originalName = names[i];
+      const modifiedName = modifiedNames[i];
+      userProfiles.push({
+        originalName,
+        modifiedName,
+        id,
+      });
+      id++;
+    }
+    return userProfiles;
+  }
+
+const names = ["Gabriel", "Clinton ", "James", "Sussana"];
+const modifiedNames = ["gabriel", "Clinton", "James", "sussana”]
+const profiles = createUserProfiles(names, modifiedNames);
+
+console.log(profiles);
